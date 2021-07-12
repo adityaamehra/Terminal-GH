23a73e5b83620c7e69fc4f1083b08669243409f6
Adityaa Mehra <adirakash@gmail.com>
2021-07-12 12:36:10 +0530

    Add all files using Terminal

---
+++ Terminal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Terminal
+//
+//  Created by Adityaa Mehra on 12/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Terminal/TerminalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TerminalApp.swift
+//  Terminal
+//
+//  Created by Adityaa Mehra on 12/07/21.
+//
+
+import SwiftUI
+
+@main
+struct TerminalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
